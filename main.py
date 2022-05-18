7d7fbe7be9ea0919b59913be1ca42f8311aa2e38
justctrlaltdel <102332459+justctrlaltdel@users.noreply.github.com>
2022-05-18 20:10:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+light_is_on = True
+
+if light_is_on:
+  print("The light is on!")
+  
+else:
+  print("We're in the dark")
+
+
+
+age = 25
+if age >= 18:
+  print ("Adult")
+else:
+  print ("Child")
+  
